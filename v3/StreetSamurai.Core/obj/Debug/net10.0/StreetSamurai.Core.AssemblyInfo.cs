31459978c531a24e25d12670ec8bc65c7dbc41ae
Ryan DeBraal <ryandebraal@gmail.com>
2026-04-05 18:58:58 -0500

    Add 3 file(s); Update 98 file(s)

--- v3/StreetSamurai.Core/obj/Debug/net10.0/StreetSamurai.Core.AssemblyInfo.cs
+++ v3/StreetSamurai.Core/obj/Debug/net10.0/StreetSamurai.Core.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("StreetSamurai.Core")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+39d8b5ce1abf933ba53af05f9d5588d11c0a9803")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cafbe2007f4a70d7b26201cb991b9fac17aab774")]
 [assembly: System.Reflection.AssemblyProductAttribute("StreetSamurai.Core")]
 [assembly: System.Reflection.AssemblyTitleAttribute("StreetSamurai.Core")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
