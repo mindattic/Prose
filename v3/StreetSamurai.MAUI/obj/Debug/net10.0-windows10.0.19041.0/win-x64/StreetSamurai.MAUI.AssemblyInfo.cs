92341e50d7fbde0cb3cf00fcf8a62c1e0ae56e85
Ryan DeBraal <ryandebraal@gmail.com>
2026-04-10 22:02:50 -0500

    deploy test 1002

--- v3/StreetSamurai.MAUI/obj/Debug/net10.0-windows10.0.19041.0/win-x64/StreetSamurai.MAUI.AssemblyInfo.cs
+++ v3/StreetSamurai.MAUI/obj/Debug/net10.0-windows10.0.19041.0/win-x64/StreetSamurai.MAUI.AssemblyInfo.cs
@@ -18,7 +18,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("StreetSamurai.MAUI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+44e899bef42a59667b023a12112acfd8cf234c25")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+378cd22e2b68806d900355f4f93c25d5a6ccdb14")]
 [assembly: System.Reflection.AssemblyProductAttribute("StreetSamurai.MAUI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("StreetSamurai.MAUI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
