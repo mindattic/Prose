61bc40f350780f1cdc5374d342bff5e32496e978
Ryan DeBraal <ryandebraal@gmail.com>
2026-04-16 08:10:47 -0500

    commit

--- v3/StreetSamurai.Blazor/obj/Debug/net10.0/StreetSamurai.Blazor.AssemblyInfo.cs
+++ v3/StreetSamurai.Blazor/obj/Debug/net10.0/StreetSamurai.Blazor.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("StreetSamurai.Blazor")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+587c70d8521d4ceb5445b7fc0c61f3c3cb102db2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+73c1ef968f1d51bcb76c11b20190759d849f121a")]
 [assembly: System.Reflection.AssemblyProductAttribute("StreetSamurai.Blazor")]
 [assembly: System.Reflection.AssemblyTitleAttribute("StreetSamurai.Blazor")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
